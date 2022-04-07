bd81a546648d8c6bc4a46f2dfcba8f7ff90d23ec
Joel Falcou <joel.falcou@lri.fr>
2022-04-07 17:27:40 +0200

    Fix #5 - Mass extractors for keyword and values

---
+++ test/extract_types.cpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/**
+  RABERU - Fancy Parameters Library
+  Copyright : RABERU Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#define TTS_MAIN
+#include "common.hpp"
+#include <raberu.hpp>
+#include <tts/tts.hpp>
+#include <variant>
+#include <tuple>
+
+TTS_CASE("Check rbr::keywords")
+{
+  using namespace rbr::literals;
+
+  auto opt = rbr::settings( "test"_kw = 1, "second"_kw = 3.5f, "last"_kw = 'z');
+  using opt_t = decltype(opt);
+
+  TTS_TYPE_IS ( rbr::result::keywords_t<opt_t>
+              , (rbr::types< decltype("test"_kw), decltype("second"_kw), decltype("last"_kw) >)
+              );
+
+  TTS_EXPR_IS ( (std::tuple{"test"_kw, "second"_kw, "last"_kw})
+              , (rbr::result::keywords_t<opt_t, std::tuple>)
+              );
+
+  TTS_EQUAL( (std::tuple{"test"_kw, "second"_kw, "last"_kw}), (rbr::keywords<std::tuple>(opt)));
+};
+
+TTS_CASE("Check rbr::values")
+{
+  using namespace rbr::literals;
+
+  auto opt = rbr::settings( "test"_kw = 1, "second"_kw = 3.5f, "last"_kw = 'z');
+  using opt_t = decltype(opt);
+
+  TTS_TYPE_IS( rbr::result::values_t<opt_t>               , (rbr::types<int,float,char>)  );
+  TTS_TYPE_IS( (rbr::result::values_t<opt_t,std::variant>), (std::variant<int,float,char>));
+  TTS_EQUAL  ( (std::tuple{1,3.5f,'z'})                   , (rbr::values<std::tuple>(opt)));
+};

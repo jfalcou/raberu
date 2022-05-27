569a0e5613f82454f51d79fd96a8becc191c83d8
Joel Falcou <joel.falcou@lri.fr>
2022-05-27 17:36:56 +0200

    Prepare Doxgen tests and setup

---
+++ test/doc.cmake
@@ -0,0 +1,7 @@
+##==================================================================================================
+##  RABERU - Cmpact C++20 Tuple Toolbox
+##  Copyright : RABERU Contributors & Maintainers
+##  SPDX-License-Identifier: MIT
+##==================================================================================================
+
+generate_test("doc/accept.cpp")

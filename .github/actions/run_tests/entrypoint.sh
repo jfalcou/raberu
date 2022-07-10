f1bcbc9c8859184b96ea6b1ab2254632103858e9
Joel Falcou <joel.falcou@lri.fr>
2022-07-10 18:32:21 +0200

    Simplify test entrypoint

--- .github/actions/run_tests/entrypoint.sh
+++ .github/actions/run_tests/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
+set -e
 
 echo "::group::Running: 'cmake .. -G Ninja -DCMAKE_CXX_FLAGS="$1" $2'"
 mkdir build
@@ -7,21 +8,11 @@ cmake .. -G Ninja -DCMAKE_CXX_COMPILER="$1"
 echo "::endgroup::"
 
 echo "::group::Compiling test" ;
-ninja -k 0 ;
-if [ "$?" -ne "0" ]
-then
-  echo "::error Tests can not be compiled!" ;
-  exit 1;
-fi
+ninja -k 0
 echo "::endgroup::" ;
 
 echo "::group::Running test" ;
-ctest ;
-if [ "$?" -ne "0" ]
-then
-  echo "::error Errors running tests!" ;
-  exit 1;
-fi
+ctest
 echo "::endgroup::" ;
 
 exit 0;

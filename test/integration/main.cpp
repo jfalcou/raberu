4bd2b7852c069f1369362d040ffe188d1877b5b9
Joel Falcou <joel.falcou@lri.fr>
2023-08-15 19:31:04 +0200

    Integration tests

---
+++ test/integration/main.cpp
@@ -0,0 +1,16 @@
+#include <raberu/raberu.hpp>
+#include <iostream>
+#include <string>
+
+template<rbr::concepts::option P0, rbr::concepts::option P1>
+auto replicate( P0 p0, P1 p1 )
+{
+  auto const params = rbr::settings(p0,p1);
+  return std::string( params["replication"_kw], params["letter"_kw] );
+}
+
+int main()
+{
+  std::cout << replicate("replication"_kw = 9, "letter"_kw = 'z'   ) << "\n";
+  std::cout << replicate("letter"_kw = '!'   , "replication"_kw = 3) << "\n";
+}

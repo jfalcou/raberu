822009ae35593a6a02136fec21d328265fa7abfd
Joel Falcou <joel.falcou@lri.fr>
2025-03-16 11:06:34 +0100

    More doc fixes

--- test/doc/checked.cpp
+++ test/doc/checked.cpp
@@ -12,8 +12,10 @@ template<typename T>
 struct is_big : std::bool_constant<(sizeof(T) > 2)> {};
 
 inline constexpr auto large_value = rbr::keyword("large"_id, rbr::if_<is_big>);
+inline constexpr auto char_value  = rbr::keyword("character"_id, rbr::only<char>);
 
 int main()
 {
-  std::cout << rbr::settings( large_value = 9.6 );
+  std::cout << rbr::settings( large_value = 9.6 ) << "\n";
+  std::cout << rbr::settings( char_value  = 'Z' ) << "\n";
 }

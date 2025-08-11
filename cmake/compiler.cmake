aa35633a0b0ec9fa905565d57019eef4c2584948
Joel Falcou <joel.falcou@lri.fr>
2025-08-11 15:43:52 +0200

    Move standalone file generation to its own branch

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -26,7 +26,7 @@ endif()
 
 target_include_directories( raberu_test INTERFACE
                             ${PROJECT_SOURCE_DIR}/test
-                            ${PROJECT_SOURCE_DIR}/standalone
+                            ${PROJECT_SOURCE_DIR}/include
                           )
 
 target_link_libraries(raberu_test INTERFACE tts::tts)
